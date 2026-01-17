7e391b2b9fd8550c5f83f00b2e657b053e50d7c6
joenash156 <joenash156@gmail.com>
2026-01-17 01:21:04 +0000

    fixed the validator of create product

--- backend/src/controllers/productsControllers.ts
+++ backend/src/controllers/productsControllers.ts
@@ -2,8 +2,34 @@ import db from "../configs/database";
 import { Request, Response } from "express"
 import { ResultSetHeader, RowDataPacket } from "mysql2"; 
 import { ZodError } from "zod";
+import { createProductSchema } from "../validators/products.schema";
 
 
 export const createProduct = async (req: Request, res: Response): Promise<void> => {
-  
+  try {
+    // validate inputs from request body
+    const validateProductData = createProductSchema.parse(req.body);
+
+    const { name, price, category_id, description, stock, image_url } = validateProductData;
+
+    
+
+  } catch(err: unknown) {
+      // check if the error comes from zod
+      if (err instanceof ZodError) {
+        res.status(400).json({
+          success: false,
+          error: "Invalid request data",
+          issues: err.issues,
+        });
+        return;
+      } 
+
+      console.error("Failed to create product: ", err);
+      res.status(500).json({
+        success: false,
+        error: "Internal server error whiles creating product!"
+      });
+      return
+  }
 }
--- backend/src/validators/products.schema.ts
+++ backend/src/validators/products.schema.ts
@@ -1,2 +1,74 @@
 import { z } from "zod";
 import { capitalizeName } from "../utils/normalize";
+import { isUUID } from "../utils/checkID";
+
+export const createProductSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, "Product name must be at least 2 characters")
+    .max(100, "Product name cannot exceed 100 characters")
+    .transform(capitalizeName),
+
+  description: z
+    .string()
+    .trim()
+    .max(500, "Description cannot exceed 500 characters")
+    .optional(),
+
+  price: z
+    .number("Price must be a number" )
+    .positive("Price must be greater than zero"),
+
+  stock: z
+    .number()
+    .int("Stock must be an integer")
+    .min(0, "Stock cannot be negative")
+    .optional()
+    .default(0),
+
+  category_id: z
+    .string()
+    .refine((id) => isUUID(id), { message: "Invalid category ID" }),
+
+  image_url: z
+    .url("Image URL must be a valid URL")
+    .optional()
+});
+
+export const updateProductBaseSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, "Product name must be at least 2 characters")
+    .max(100, "Product name cannot exceed 100 characters")
+    .transform(capitalizeName)
+    .optional(),
+
+  description: z
+    .string()
+    .trim()
+    .max(500, "Description cannot exceed 500 characters")
+    .optional(),
+
+  price: z
+    .number("Price must be a number" )
+    .positive("Price must be greater than zero")
+    .optional(),
+
+  stock: z
+    .number()
+    .int("Stock must be an integer")
+    .min(0, "Stock cannot be negative")
+    .optional()
+    .default(0),
+  
+  category_id: z
+  .string()
+  .refine((id) => isUUID(id), { message: "Invalid category ID" })
+  .optional(),
+
+  image_url: z
+    .url("Image URL must be a valid URL")
+    .optional()
+});
